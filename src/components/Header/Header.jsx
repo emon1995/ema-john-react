b59a8e373484f99552778385d54a3c708c2e76f3
emon1995 <emonhossain995@gmail.com>
2023-03-28 16:33:52 +0600

    some file added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,72 +1,19 @@
-import React from "react";
-import logo from "../../assets/images/Logo.svg";
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
 
 const Header = () => {
-  return (
-    <div className=" ">
-      <div className="navbar bg-neutral px-12 text-white">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a href="/order">Order</a>
-              </li>
-              <li tabIndex={0}>
-                <a href="/review">Order Review</a>
-              </li>
-
-              <li>
-                <a href="/inventory">Manage Inventory</a>
-              </li>
-              <li>
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/order">Orders</a>
+                <a href="/inventory">Inventory</a>
                 <a href="/login">Login</a>
-              </li>
-            </ul>
-          </div>
-          <a href="/" className="btn btn-ghost normal-case text-xl">
-            <img src={logo} alt="logo" />
-          </a>
-        </div>
-        <div className="navbar-end">
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <a href="/order">Order</a>
-              </li>
-              <li tabIndex={0}>
-                <a href="/review">Order Review</a>
-              </li>
-              <li>
-                <a href="/inventory">Manage Inventory</a>
-              </li>
-              <li>
-                <a href="/login">Login</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+            </div>
+        </nav>
+    );
 };
 
 export default Header;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,52 +1,40 @@
-import React, { useEffect, useState } from "react";
-import allProducts from "../fakeData/products.json";
-import Product from "../Proudct/Product";
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
 
-  const handleClick = (product) => {
-    // console.log(product);
-    const newProduct = [...cart, product];
-    setCart(newProduct);
-  };
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
 
-  useEffect(() => {
-    fetch(
-      " https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        setProducts(data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
+    const handleAddToCart = (product) => {
+        // cart.push(product); 
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
-  return (
-    <div className="flex flex-col-reverse gap-4  lg:flex-row lg:justify-between ">
-      {/* PRODUCTS */}
-      <div className="  lg:w-[70%]  mx-4 lg:p-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-4">
-        {products.slice(0, 10).map((product) => (
-          <Product
-            key={product.id}
-            product={product}
-            handleClick={handleClick}
-          ></Product>
-        ))}
-      </div>
-      {/* ORDER SUMMARY */}
-      <div className="w-full lg:w-[25%] bg-primary p-4 mt-4 lg:mt-0 lg:mx-0 ">
-        <h1 className="text-center text-white text-2xl font-bold my-8">
-          Order Summary
-        </h1>
-        <p className="text-white font-bold">Selected Items: {cart.length}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Shop;
