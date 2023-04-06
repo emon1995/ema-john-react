3928c9004551f0e1dcc1f6ed2a604cd7386f7a7c
emon1995 <emonhossain995@gmail.com>
2023-04-06 21:12:19 +0600

    Basic router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import Shop from './components/Shop/Shop'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <Header></Header>
-      <Shop></Shop>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders";
+import Inventory from "./components/Inventory/Inventory";
+import Login from "./components/Login/Login";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "/orders",
+        element: <Orders></Orders>,
+      },
+      {
+        path: "/inventory",
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
