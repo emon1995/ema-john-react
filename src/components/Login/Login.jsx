98b8b9e36b8d0a864575209db5f3ea7ef2c45192
emon1995 <emonhossain995@gmail.com>
2023-04-17 16:34:28 +0600

    Login & Register ui design added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type={"password"} name="password" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+      <p>
+        <small>
+          New to Ema-john? <Link to="/register">Create New Account</Link>
+        </small>
+      </p>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import "./Register.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+    console.log(confirm, email, password);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="form-container">
+      <h2 className="form-title">Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
+      </form>
+      <p>
+        <small>
+          Already have an account? <Link to="/login">Login</Link>
+        </small>
+      </p>
+      {/* <p className="text-error">{error}</p> */}
     </div>
   );
 };
